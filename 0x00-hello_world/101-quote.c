eb3fdc116090717e463cfff0f6b1500bb966ae66
Tobi <omoniyiadejengbe@gmail.com>
2023-01-04 00:16:10 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  *
+  * main - entry point
+  *
+  * return 1 for success
+*/
+int main(void)
+{
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
