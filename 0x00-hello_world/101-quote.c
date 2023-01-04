92bdff76442ab6b3ff806346efd1bacc803cc472
Tobi <omoniyiadejengbe@gmail.com>
2023-01-04 00:25:04 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
   *
   * main - entry point
@@ -7,6 +7,6 @@
 */
 int main(void)
 {
-	fwrite(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
